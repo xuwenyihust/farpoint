0a42abef8848d48a0c30a0018c3a8b33a6ca84ca
Wenyi Xu <wenyixu101@gmail.com>
2026-08-03 21:04:43 +0800

    fix: center cylinder sidewall grasp

--- tests/test_shape_position.py
+++ tests/test_shape_position.py
@@ -68,7 +68,7 @@ def test_apply_trial_reuses_template_but_changes_task_identity_and_shape():
         "static_friction": 2.5,
         "dynamic_friction": 2.0,
         "finger_contact_max_effort": 20.0,
-        "grasp_height_offset_meters": -0.005,
+        "grasp_height_offset_meters": 0.0,
         "grasp_aperture_bias_xy": [0.0, 0.0],
         "grasp_tracking_max_xy_error": 0.006,
         "grasp_descent_max_xy_error": 0.006,
