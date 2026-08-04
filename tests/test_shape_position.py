4d2bb9bf69b8413bcfa487e45974bbd9f837aa07
Wenyi Xu <wenyixu101@gmail.com>
2026-08-04 10:46:58 +0800

    fix: recenter the cylinder between both fingers

--- tests/test_shape_position.py
+++ tests/test_shape_position.py
@@ -81,7 +81,7 @@ def test_apply_trial_reuses_template_but_changes_task_identity_and_shape():
         "tactile_close_step_radians": 0.00005,
         "unilateral_recenter_enabled": True,
         "unilateral_recenter_step": 0.00005,
-        "unilateral_recenter_max_correction": 0.003,
+        "unilateral_recenter_max_correction": 0.008,
         "unilateral_recenter_persistence_frames": 12,
         "bilateral_contact_min_finger_position_radians": 0.02,
         "grasp_validation_frames": 1,
